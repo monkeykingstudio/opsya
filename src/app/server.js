41decad7c47fdd5302a0366ba2b30b207572a032
raphael <raphael.vareilles@gmail.com>
2022-04-25 12:12:47 +0200

    first deploy

--- src/app/server.js
+++ src/app/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/nameless-fjord-55334'));
+app.use(express.static('./dist/opsya-dapp'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/nameless-fjord-55334/'}),
+    res.sendFile('index.html', {root: 'dist/opsya-dapp/'}),
 );
 
 // Start the app by listening on the default Heroku port
